4297d54f0bc2c3303a2e1bfee086fae02179d092
Paul <36346345+Pablo-de-Santa@users.noreply.github.com>
2019-04-07 15:19:57 +0300

    Add files via upload
    ready without footer and media

---
+++ Audi_poject/info/info.js
@@ -0,0 +1,63 @@
+//SLIDER
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// END OF SLIDER
+
+//AOS
+
+AOS.init();
+
+
+// SCROLL TO TOP
+
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//For movimg pic
+
+type = "text/javascript"
+$('section').mousemove(function(e){
+  var moveX = (e.pageX * -1/100);
+  var moveY = (e.pageY * 1/100);
+ $(this).css('background-position', moveX + 'px ' + moveY + 'px')
+})
